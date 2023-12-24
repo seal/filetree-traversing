893105d7073b4936c09c02c9ab5c9c2fdab9c5c5
will <zseal75@gmail.com>
2023-12-24 19:36:07 +0000

    Changes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
+use rayon::prelude::*;
 use std::fs;
+use std::path::{Path, PathBuf};
 use std::time::Instant;
 
 const MAX_DEPTH: usize = 5;
 
 #[derive(Debug, Clone)]
 struct TreeNode {
-    data: String,
-    children: Vec<Box<TreeNode>>,
+    data: PathBuf,
+    children: Vec<TreeNode>,
 }
 
 impl TreeNode {
-    fn new(data: String) -> Self {
+    fn new(data: PathBuf) -> Self {
         TreeNode {
             data,
             children: Vec::new(),
@@ -18,21 +20,45 @@ impl TreeNode {
     }
 
     fn add_child(&mut self, child: TreeNode) {
-        self.children.push(Box::new(child));
+        self.children.push(child);
     }
 
-    fn build_tree(root_path: &str, depth: usize) -> TreeNode {
+    fn build_tree_parallel(root_path: &Path, depth: usize) -> TreeNode {
         if depth == 0 {
-            return TreeNode::new("...".to_string());
+            return TreeNode::new(PathBuf::from("..."));
         }
 
-        let mut root = TreeNode::new(root_path.to_string());
+        let mut root = TreeNode::new(root_path.to_path_buf());
+        if let Ok(entries) = fs::read_dir(root_path) {
+            let children: Vec<TreeNode> = entries
+                .par_bridge()
+                .map(|entry| {
+                    if let Ok(entry) = entry {
+                        TreeNode::build_tree(&entry.path(), depth - 1)
+                    } else {
+                        TreeNode::new(PathBuf::from("..."))
+                    }
+                })
+                .collect();
+
+            for child in children {
+                root.add_child(child);
+            }
+        }
+        root
+    }
+
+    fn build_tree(root_path: &Path, depth: usize) -> TreeNode {
+        if depth == 0 {
+            return TreeNode::new(PathBuf::from("..."));
+        }
+
+        let mut root = TreeNode::new(root_path.to_path_buf());
         if let Ok(entries) = fs::read_dir(root_path) {
             for entry in entries {
                 if let Ok(entry) = entry {
                     let path = entry.path();
-                    let data = path.to_str().unwrap().to_string();
-                    let child = TreeNode::build_tree(&data, depth - 1);
+                    let child = TreeNode::build_tree(&path, depth - 1);
                     root.add_child(child);
                 }
             }
@@ -40,30 +66,26 @@ impl TreeNode {
         root
     }
 
-    fn depth_first_traversal(&self) {
-        self.depth_first_traversal_recursive("");
-    }
-
     fn depth_first_traversal_recursive(&self, prefix: &str) {
-        println!("{}{}", prefix, self.data);
+        println!("{}{:?}", prefix, self.data.display());
 
-        for (i, child) in self.children.iter().enumerate() {
-            let is_last = i == self.children.len() - 1;
-            let new_prefix = format!("{}{}", prefix, if is_last { "   " } else { "|  " });
-            child.depth_first_traversal_recursive(&new_prefix);
+        for (index, child) in self.children.iter().enumerate() {
+            let child_prefix = if index == self.children.len() - 1 {
+                format!("{}└──", prefix)
+            } else {
+                format!("{}├──", prefix)
+            };
+
+            child.depth_first_traversal_recursive(&format!("{} ", child_prefix));
         }
     }
 }
 
 fn main() {
-    let start_time = Instant::now();
-    let root = TreeNode::build_tree("/home/will", MAX_DEPTH);
-
-    let elapsed_time = start_time.elapsed();
-    println!("Tree built in: {:?}", elapsed_time);
-    let start_time = Instant::now();
-    root.depth_first_traversal();
-    let elapsed_time = start_time.elapsed();
-    println!("Traversal completed in: {:?}", elapsed_time);
+    let start_time2 = Instant::now();
+    let root2 = TreeNode::build_tree_parallel(Path::new("/"), MAX_DEPTH);
+    let tree_build_parallel = start_time2.elapsed();
+    root2.depth_first_traversal_recursive("-");
+    println!("Parallel Tree built in: {:?}", tree_build_parallel);
 }
 
