d6ad989f7b39bfd487da36924d8d85ce71d5b95f
will <zseal75@gmail.com>
2023-12-24 22:21:22 +0000

    Change to file explorer

--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,51 @@
-use rayon::prelude::*;
-use std::fs;
-use std::path::{Path, PathBuf};
-use std::time::Instant;
+use colored::Colorize;
 
-const MAX_DEPTH: usize = 5;
+use std::{fs, io};
 
-#[derive(Debug, Clone)]
-struct TreeNode {
-    data: PathBuf,
-    children: Vec<TreeNode>,
-}
+fn menu(base_path: &str) -> io::Result<()> {
+    let mut exit = false;
 
-impl TreeNode {
-    fn new(data: PathBuf) -> Self {
-        TreeNode {
-            data,
-            children: Vec::new(),
-        }
-    }
+    while !exit {
+        for entry in fs::read_dir(base_path)? {
+            let entry = entry?;
+            let file_path = entry.path();
+            let file_metadata = fs::metadata(&file_path)?;
 
-    fn add_child(&mut self, child: TreeNode) {
-        self.children.push(child);
-    }
-
-    fn build_tree_parallel(root_path: &Path, depth: usize) -> TreeNode {
-        if depth == 0 {
-            return TreeNode::new(PathBuf::from("..."));
-        }
-
-        let mut root = TreeNode::new(root_path.to_path_buf());
-        if let Ok(entries) = fs::read_dir(root_path) {
-            let children: Vec<TreeNode> = entries
-                .par_bridge()
-                .map(|entry| {
-                    if let Ok(entry) = entry {
-                        TreeNode::build_tree(&entry.path(), depth - 1)
-                    } else {
-                        TreeNode::new(PathBuf::from("..."))
-                    }
-                })
-                .collect();
-
-            for child in children {
-                root.add_child(child);
-            }
-        }
-        root
-    }
-
-    fn build_tree(root_path: &Path, depth: usize) -> TreeNode {
-        if depth == 0 {
-            return TreeNode::new(PathBuf::from("..."));
-        }
-
-        let mut root = TreeNode::new(root_path.to_path_buf());
-        if let Ok(entries) = fs::read_dir(root_path) {
-            for entry in entries {
-                if let Ok(entry) = entry {
-                    let path = entry.path();
-                    let child = TreeNode::build_tree(&path, depth - 1);
-                    root.add_child(child);
-                }
-            }
-        }
-        root
-    }
-
-    fn depth_first_traversal_recursive(&self, prefix: &str) {
-        println!("{}{:?}", prefix, self.data.display());
-
-        for (index, child) in self.children.iter().enumerate() {
-            let child_prefix = if index == self.children.len() - 1 {
-                format!("{}└──", prefix)
+            let file_name = entry.file_name();
+            if !file_metadata.is_file() {
+                println!("{} ", file_name.to_string_lossy().blue(),);
             } else {
-                format!("{}├──", prefix)
-            };
+                println!("{} {}", file_name.to_string_lossy(), file_metadata.len());
+            }
+        }
 
-            child.depth_first_traversal_recursive(&format!("{} ", child_prefix));
+        println!("Enter file name to navigate to");
+        println!("Enter 'exit' to quit or press Enter to continue:");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        let input = input.trim(); // Remove leading and trailing whitespaces
+
+        if input == "exit" {
+            exit = true;
+        } else {
+            menu(&(base_path.to_owned() + input + "/"))?;
         }
     }
+
+    Ok(())
 }
 
 fn main() {
-    let start_time2 = Instant::now();
-    let root2 = TreeNode::build_tree_parallel(Path::new("/"), MAX_DEPTH);
-    let tree_build_parallel = start_time2.elapsed();
-    root2.depth_first_traversal_recursive("-");
-    println!("Parallel Tree built in: {:?}", tree_build_parallel);
+    match menu("/") {
+        Ok(()) => {
+            // Handle success if needed
+            println!("Menu executed successfully");
+        }
+        Err(err) => {
+            // Handle error
+            eprintln!("Error: {}", err);
+        }
+    }
 }
 
