93160df6688f4c92ae7629348e07ca382d4f85a8
seal <will@kimbell.uk>
2024-06-12 17:16:46 +0100

    release: v0.1.14

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ use std::{fs, io};
 /// ```
 pub fn menu(base_path: &str) -> io::Result<()> {
     let mut exit = false;
-    // altered 1
+    // altered 10
     while !exit {
         for entry in fs::read_dir(base_path)? {
             let entry = entry?;
